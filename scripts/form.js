95c01a7192ebe62828a1eab1ed0af06891ea1e7d
marsil1990 <ma.silveram@gmail.com>
2025-07-29 09:07:12 -0300

    week5

--- scripts/form.js
+++ scripts/form.js
@@ -44,3 +44,6 @@ renderOptions(products);
 
 const lastModified = document.lastModified;
 document.getElementById("lastModified").textContent = "Last Modified " + lastModified;
+const yearSpan = document.getElementById("currentyear");
+const currentYear = new Date().getFullYear();
+yearSpan.textContent = currentYear;
